eb7b7739edcb398c279c3553f2acdba5aed5f551
huanglongyun <974859862@qq.com>
2026-06-06 11:06:10 +0800

    chore: init java gateway with Spring Boot skeleton

---
+++ java-gateway/src/main/java/com/esslice/EssliceApplication.java
@@ -0,0 +1,11 @@
+package com.esslice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EssliceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EssliceApplication.class, args);
+    }
+}
